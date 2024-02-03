3cad6031b24d3210743c4491310091c9d0c46d05
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-02-04 01:28:55 +0600

    recap of variable types, conditional, Array, loop, object, function

---
+++ recap.js
@@ -0,0 +1,69 @@
+/**
+ * ------------- variable types --------------
+ * 
+ * 1. const --> value can't change
+ * 2. let --> block scope and value can be change
+ * 3. var --> global scope and value can be change
+ * 
+ * _______________ primitive _______________
+ * 1. null
+ * 2. undefined
+ * 3. Number
+ * 4. String
+ * 5. boolean
+ * 
+ * _____________ non-primitive _____________
+ * 1. Array
+ * 2. Object
+ */
+
+
+/**
+ * ---------------- conditional -----------------
+ * ==> if, if-else, if-else if, else, etc
+ * 
+ * 1. conditional operator: >, <, >=, <=, ===, !==
+ * 
+ * 2. logical operator: &&, ||
+ */
+
+
+/**
+ * ---------------- Array ------------------
+ * 1. [0, 1, 2, 3, 4, ... index]
+ * 2. push - pop
+ * 3. shift - unshift
+ * 4. includes
+ * 5. filter
+ * 6. join
+ * 7. concat
+ * 8. indexOf
+ * 9. length
+ * 10. isArray
+ * 11. slice
+ */
+
+
+/**
+ * --------------- loop ------------------
+ * 1. for
+ * 2. white
+ * 3. do - while
+ * 4. for-of
+ * 5. for-in 
+ */
+
+
+/**
+ * -------------- object ----------------
+ * 1. for - in
+ * 2. keys and values
+ */
+
+
+/**
+ * ----------- function -------------
+ * 1. declaration with parameter
+ * 2. function calls with argument
+ * 3. return sometimes
+ */
