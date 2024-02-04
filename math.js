964a1aa53b650484c443d67dfa315c22b339f006
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-02-04 19:37:21 +0600

    js basic Math.properties

---
+++ math.js
@@ -0,0 +1,39 @@
+/**
+ * ------ in-built Math properties ------
+ * 
+ * ==> Math.min:  to define min number
+ * ==> Math.max:  to define max number
+ * ==> Math.PI: pi value
+ * ==> Math.abs: to get absolute value
+ * ==> Math.round: get nearest integer
+ * ==> Math.floor: get small nearest integer
+ * ==> Math.ceil: get big nearest integer
+ * ==> Math.random: give a random number between 0 to 1
+ */
+
+// Math.min
+const min = Math.min(45, 21, 65, 99, 126, 5 -99);
+console.log('min:' ,min, '\n');
+
+// Math.max
+const max = Math.max(45, 21, 65, 99, 126, 5 -99);
+console.log('max:', max, '\n');
+
+// Math.PI
+console.log('pi:', Math.PI, '\n');
+
+// Math.abs
+console.log('absolute value:', Math.abs(-44), '\n');
+console.log('absolute value:', Math.abs(44.347), '\n');
+
+// Math.round
+console.log('round:', Math.round(-2.4999), '\n');
+
+// Math.floor
+console.log('round:', Math.floor(-2.4999), '\n');
+
+// Math.ceil
+console.log('round:', Math.ceil(-2.4999), '\n');
+
+// Math.random
+console.log('random:', Math.random(), '\n');
